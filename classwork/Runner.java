f36c0eef84d6d5898762159ab669af0d080fc84a
Arjun Patrawala <ajpat1234@gmail.com>
2019-03-04 14:30:29 -0800

    classwork

---
+++ classwork/Runner.java
@@ -0,0 +1,9 @@
+public class Runner {
+  public static void main(String[] args) {
+    BinaryTree<Integer> bt = new BinaryTree<Integer>();
+    bt.printInOrder();
+    bt.printPreOrder();
+    bt.printPostOrder();
+    bt.printReverseOrder();
+  }
+}
