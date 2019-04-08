fddb88fcd6805643d99cbf0bff70c5c8828734af
Arjun Patrawala <ajpat1234@gmail.com>
2019-04-08 14:21:25 -0700

    classwork

--- classwork/Runner.java
+++ classwork/Runner.java
@@ -1,23 +1,16 @@
-import java.util.Scanner;
-
 public class Runner {
   public static void main(String[] args) {
-    MinHeap<Task> heap = new MinHeap<Task>();
-
-    Scanner kb = new Scanner(System.in);
+    MaxHeap<Integer> heap = new MaxHeap<Integer>();
 
     int num = 1;
-    while (num < 6) {
-      System.out.println("Enter a task");
-      String task = kb.next();
-
-      System.out.println("Enter a priority");
-      int priority = kb.nextInt();
-
-      heap.add(new Task(task, priority));
+    while (num < 9) {
+      int num2 = (int)(Math.random() * 100 + 1);
+      heap.add(num2);
 
       num++;
     }
+
+    System.out.println(heap.toString());
 
     while (heap.size() > 0) {
       System.out.println(heap.poll());
