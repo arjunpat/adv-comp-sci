f33ff64c7877074c48ac2903954b1f825ccef821
Arjun Patrawala <ajpat1234@gmail.com>
2019-04-29 13:58:58 -0700

    classwork 2

--- classwork/Box.java
+++ classwork/Box.java
@@ -7,33 +7,38 @@ public class Box implements Runnable {
   private int x;
   private int y;
   private boolean right = true;
-  public Box() {
+  private Screen s;
+  public Box(Screen s) {
     x = 0;
     y = 40;
+    this.s = s;
   }
 
   public void run() {
-    if (x>700) {
-      right = false;
-    }
+    while (true) {
+      if (x>700)
+        right = false;
 
-    if (x<0) {
-      right = true;
-    }
+      if (x<0) {
+        right = true;
+      }
 
-    if (right) {
-      x+=10;
-    } else {
-      x-=10;
-    }
+      if (right) {
+        x+=10;
+      } else {
+        x-=10;
+      }
 
-    try {
-      Thread.sleep(10);
-    } catch(InterruptedException e) {}
+      try {
+        Thread.sleep(10);
+      } catch(InterruptedException e) {}
+
+        s.repaint();
+    }
   }
 
   public void draw(Graphics g) {
-    g.setColor(Color.YELLOW);
+    g.setColor(Color.RED);
     g.fillRect(x, y, 100, 100);
   }
 
--- classwork/Screen.java
+++ classwork/Screen.java
@@ -4,26 +4,15 @@ import java.awt.event.*;
 import java.util.*;
 
 public class Screen extends View {
-  private Box box = new Box();
+  private Box box = new Box(this);
   Thread boxThread = new Thread(box);
 
   public Screen() {
-
     boxThread.start();
-
-    Thread t = new Thread(new Runnable() {
-      public void run() {
-        try { Thread.sleep(10); } catch(Exception e) {}
-        repaint();
-      }
-    });
-
-    t.start();
   }
 
   public void draw(Graphics g) {
     box.draw(g);
-
   }
 
 }
